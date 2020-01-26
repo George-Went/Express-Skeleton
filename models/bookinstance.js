a711070f58e84a4322a74bc7077ab38fd2998f82
George <gep.went@gmail.com>
2020-01-26 17:59:02 +0000

    Add testing data and displaying books

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
+var moment = require('moment');
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
@@ -20,5 +21,12 @@ BookInstanceSchema
   return '/catalog/bookinstance/' + this._id;
 });
 
-//Export model
+// Virtual date formatting val.due_back_formatted
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
+});
+
+// Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
